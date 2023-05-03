08e6bbd7604dcd73d11032877f7bc1d8686d21e7
Allen Chen <allen37751@gmail.com>
2023-05-03 16:52:31 +0800

    create app

---
+++ src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+  
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>
+        儀表板
+      </h1>
+    </div>
+  );
+};
+  
+export default Dashboard;
---
+++ src/components/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+  
+const Login = () => {
+  return (
+    <div>
+      <h1>
+        登入
+      </h1>
+    </div>
+  );
+};
+  
+export default Login;
